ad2d36e66a865898afbf3a2c720de8a717816121
Mathina <hbanis8@gmail.com>
2024-08-20 23:22:03 +0530

    Selenium learning

---
+++ 16082024/test_sele_Lab014.py
@@ -0,0 +1,42 @@
+import pytest
+import time
+import allure
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+@pytest.mark.postive
+def test_VWO_Login_invalid():
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    driver.get("https://app.vwo.com")
+
+    email_input = driver.find_element(By.CSS_SELECTOR, "[name= 'username']")
+    password_input = driver.find_element(By.CSS_SELECTOR, "[name= 'password']")
+
+    email_input.send_keys("admin@gmail.com")
+    password_input.send_keys("admin")
+
+    button_submit_element = driver.find_element(By.ID, "js-login-btn")
+    button_submit_element.click()
+
+    # time.sleep(5) # This is Python Int who is waiting, Python Execution Halt.
+
+    # A Condition to check the element
+    # error_msg_element - comes after 5 seconds
+    # I have to wait with some condition -
+    # wait with the condition
+    # Add a condition so that Webdriver should wait for that condition.
+    # element is visible then assertion
+    # when  this -> then do this
+
+    WebDriverWait(driver= driver,timeout=5).until(
+        EC.visibility_of_element_located((By.ID, "js-notification-box-msg"))
+    )
+    error_msg_element = driver.find_element(By.ID, "js-notification-box-msg")
+    print(error_msg_element.text)
+    assert error_msg_element.text == "Your email, password, IP address or location did not match"
+
+
